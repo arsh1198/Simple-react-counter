e6b34e8ce0a0547e9be24f0f6c1c02b079023bee
turbo <turbonater62@gmail.com>
2020-12-30 22:48:37 +0530

    First React Web App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [counter, setCounter] = useState(0);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <p className="title">Counter</p>
+      <div className="inner-div">
+        <button className="button" onClick={() => setCounter(counter + 1)}>
+          +
+        </button>
+        <h1>{counter}</h1>
+        <button className="button" onClick={() => setCounter(counter - 1)}>
+          -
+        </button>
+      </div>
     </div>
   );
 }
