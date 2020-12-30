a59272dfaece1be0e88e0551cecec90ed2436134
turbo <turbonater62@gmail.com>
2020-12-31 00:43:16 +0530

    dark mode

--- src/App.js
+++ src/App.js
@@ -1,17 +1,38 @@
+/** @jsx jsx */
+import { jsx } from "theme-ui";
 import { useState } from "react";
+import { useColorMode } from "theme-ui";
 import "./App.css";
 
 function App() {
   const [counter, setCounter] = useState(0);
+  const [colorMode, setColorMode] = useColorMode();
   return (
-    <div className="container">
-      <p className="title">Counter</p>
-      <div className="inner-div">
-        <button className="button" onClick={() => setCounter(counter + 1)}>
+    <div className="container" sx={{ backgroundColor: "background" }}>
+      <button
+        onClick={(e) => {
+          setColorMode(colorMode === "default" ? "dark" : "default");
+        }}
+      >
+        Toggle {colorMode === "default" ? "Dark" : "Light"}
+      </button>
+      <p className="title" sx={{ color: "text" }}>
+        Counter
+      </p>
+      <div className="inner-div" sx={{ backgroundColor: "card" }}>
+        <button
+          sx={{ backgroundColor: "button", color: "text" }}
+          className="button"
+          onClick={() => setCounter(counter + 1)}
+        >
           +
         </button>
         <h1>{counter}</h1>
-        <button className="button" onClick={() => setCounter(counter - 1)}>
+        <button
+          sx={{ backgroundColor: "button", color: "text" }}
+          className="button"
+          onClick={() => setCounter(counter - 1)}
+        >
           -
         </button>
       </div>
---
+++ src/theme.js
@@ -0,0 +1,19 @@
+const theme = {
+  colors: {
+    background: "#add8e6",
+    card: "#ffc0cb",
+    text: "#000",
+    border: "#000",
+    button: "#fff",
+    modes: {
+      dark: {
+        background: "#030022",
+        card: "#220030",
+        text: "#f1f1f1",
+        border: "#f1f1f1",
+        button: "#909090",
+      },
+    },
+  },
+};
+export default theme;
