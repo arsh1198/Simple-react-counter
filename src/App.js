74d2ad93b154e18833b14da467aaf0e78d74a065
turbo <turbonater62@gmail.com>
2020-12-31 14:22:47 +0530

    wrapped

--- src/App.js
+++ src/App.js
@@ -2,26 +2,42 @@
 import { jsx } from "theme-ui";
 import { useState } from "react";
 import { useColorMode } from "theme-ui";
+import Switch from "react-switch";
+import { GiMoon } from "react-icons/gi";
+import { RiSunFill } from "react-icons/ri";
 import "./App.css";
 
 function App() {
   const [counter, setCounter] = useState(0);
   const [colorMode, setColorMode] = useColorMode();
+  const checked = colorMode === "dark" ? true : false;
   return (
     <div className="container" sx={{ backgroundColor: "background" }}>
-      <button
-        onClick={(e) => {
-          setColorMode(colorMode === "default" ? "dark" : "default");
-        }}
-      >
-        Toggle {colorMode === "default" ? "Dark" : "Light"}
-      </button>
+      <div className="switch">
+        <Switch
+          onChange={() =>
+            setColorMode(colorMode === "default" ? "dark" : "default")
+          }
+          checked={checked}
+          uncheckedIcon={
+            <GiMoon size={20} style={{ marginTop: 4, marginLeft: 6 }} />
+          }
+          checkedIcon={
+            <RiSunFill size={20} style={{ marginTop: 4, marginLeft: 4 }} />
+          }
+          offColor="#ffc0cb"
+          onColor="#610035"
+          onHandleColor="#fff"
+          offHandleColor="#000"
+          handleDiameter={20}
+        />
+      </div>
       <p className="title" sx={{ color: "text" }}>
         Counter
       </p>
       <div className="inner-div" sx={{ backgroundColor: "card" }}>
         <button
-          sx={{ backgroundColor: "button", color: "text" }}
+          sx={{ backgroundColor: "button" }}
           className="button"
           onClick={() => setCounter(counter + 1)}
         >
@@ -29,7 +45,9 @@ function App() {
         </button>
         <h1>{counter}</h1>
         <button
-          sx={{ backgroundColor: "button", color: "text" }}
+          sx={{
+            backgroundColor: "button",
+          }}
           className="button"
           onClick={() => setCounter(counter - 1)}
         >
--- src/theme.js
+++ src/theme.js
@@ -7,11 +7,11 @@ const theme = {
     button: "#fff",
     modes: {
       dark: {
-        background: "#030022",
-        card: "#220030",
+        background: "#003c4f",
+        card: "#610035",
         text: "#f1f1f1",
         border: "#f1f1f1",
-        button: "#909090",
+        button: "#ffc0cb",
       },
     },
   },
